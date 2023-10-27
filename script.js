9549f27bd2d3fcf9bb086bf5c7ce16111b9cae03
Rdg94 <rodrigo_paulino_11@hotmail.com>
2023-10-27 10:23:14 -0300

    Alteração no JavaScript

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 function toggleMode() {
-const html = document.documentElement
+  const html = document.documentElement
 
-if  (html.classList.contains('light')) 
-{
-    html.classList.remove('light')
-}
-    else {
-        html.classList.add('light')
-    }
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
 
-const img = document.querySelector("#profile img")
-    if (html.classList.contains('light')) {
-        img.setAttribute('src', './images/avatar-light.png')
-    }
-    else{
-        img.setAttribute('src', './images/avatar.png')
-    }
-
-
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "images/avatar-light.png")
+  } else {
+    img.setAttribute("src", "images/avatar.png")
+  }
 }
